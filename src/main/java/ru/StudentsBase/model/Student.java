738e5976dcf1000b7976355f8592264325c69ec2
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-05-31 00:02:43 +0300

    empty project starter with builders

---
+++ src/main/java/ru/StudentsBase/model/Student.java
@@ -0,0 +1,108 @@
+package ru.StudentsBase.model;
+
+public class Student {
+
+    //Поля класса
+    private String fullName, universityId;
+    private int currentCourseNumber;
+    private float avgExamScore;
+
+    //Конструкторы
+
+
+    public Student() {
+    }
+
+    public Student(String fullName, String universityId, int currentCourseNumber, float avgExamScore) {
+        this.fullName = fullName;
+        this.universityId = universityId;
+        this.currentCourseNumber = currentCourseNumber;
+        this.avgExamScore = avgExamScore;
+    }
+
+    //Геттеры и сеттеры
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUniversityId() {
+        return universityId;
+    }
+
+    public int getCurrentCourseNumber() {
+        return currentCourseNumber;
+    }
+
+    public float getAvgExamScore() {
+        return avgExamScore;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setUniversityId(String universityId) {
+        this.universityId = universityId;
+    }
+
+    public void setCurrentCourseNumber(int currentCourseNumber) {
+        this.currentCourseNumber = currentCourseNumber;
+    }
+
+    public void setAvgExamScore(float avgExamScore) {
+        this.avgExamScore = avgExamScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "fullName='" + fullName + '\'' +
+                ", universityId='" + universityId + '\'' +
+                ", currentCourseNumber=" + currentCourseNumber +
+                ", avgExamScore=" + avgExamScore +
+                '}';
+    }
+
+    //Билдер
+    public static class StudentBuilder {
+        private String fullName;
+        private String universityId;
+        private int currentCourseNumber;
+        private float avgExamScore;
+
+        public StudentBuilder setFullName(String fullName) {
+            this.fullName = fullName;
+            return this;
+        }
+
+        public StudentBuilder setUniversityId(String universityId) {
+            this.universityId = universityId;
+            return this;
+        }
+
+        public StudentBuilder setCurrentCourseNumber(int currentCourseNumber) {
+            this.currentCourseNumber = currentCourseNumber;
+            return this;
+        }
+
+        public StudentBuilder setAvgExamScore(float avgExamScore) {
+            this.avgExamScore = avgExamScore;
+            return this;
+        }
+
+        public Student createStudent() {
+            return new Student(fullName, universityId, currentCourseNumber, avgExamScore);
+        }
+
+        @Override
+        public String toString() {
+            return "StudentBuilder{" +
+                    "fullName='" + fullName + '\'' +
+                    ", universityId='" + universityId + '\'' +
+                    ", currentCourseNumber=" + currentCourseNumber +
+                    ", avgExamScore=" + avgExamScore +
+                    '}';
+        }
+    }
+
+}
---
+++ src/main/java/ru/StudentsBase/model/University.java
@@ -0,0 +1,143 @@
+package ru.StudentsBase.model;
+
+public class University {
+
+    public University createUniversity() {
+        return new UniversityBuilder().setId(id).setFullName(fullName).setShortName(shortName).setYearOfFoundation(yearOfFoundation).setMainProfile(mainProfile).createUniversity();
+    }
+
+    //Добавляем Enum
+    public enum StudyProfile {
+
+        MEDICINE("Медицина");
+
+        String profileName;
+
+        StudyProfile(String profileName) {
+            this.profileName = profileName;
+        }
+    }
+
+    //Добавляем поля класса
+    private String id, fullName, shortName;
+    private int yearOfFoundation;
+    StudyProfile mainProfile;
+
+    //Конструкторы
+
+    public University() {
+    }
+
+    public University(String id, String fullName, String shortName, int yearOfFoundation, StudyProfile mainProfile) {
+        this.id = id;
+        this.fullName = fullName;
+        this.shortName = shortName;
+        this.yearOfFoundation = yearOfFoundation;
+        this.mainProfile = mainProfile;
+    }
+
+    //Геттеры и сеттеры
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public int getYearOfFoundation() {
+        return yearOfFoundation;
+    }
+
+    public StudyProfile getMainProfile() {
+        return mainProfile;
+    }
+
+    public University setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public University setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public University setShortName(String shortName) {
+        this.shortName = shortName;
+        return this;
+    }
+
+    public University setYearOfFoundation(int yearOfFoundation) {
+        this.yearOfFoundation = yearOfFoundation;
+        return this;
+    }
+
+    public University setMainProfile(StudyProfile mainProfile) {
+        this.mainProfile = mainProfile;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", shortName='" + shortName + '\'' +
+                ", yearOfFoundation=" + yearOfFoundation +
+                ", mainProfile=" + mainProfile +
+                '}';
+    }
+
+    public static class UniversityBuilder {
+        private String id;
+        private String fullName;
+        private String shortName;
+        private int yearOfFoundation;
+        private University.StudyProfile mainProfile;
+
+        public UniversityBuilder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public UniversityBuilder setFullName(String fullName) {
+            this.fullName = fullName;
+            return this;
+        }
+
+        public UniversityBuilder setShortName(String shortName) {
+            this.shortName = shortName;
+            return this;
+        }
+
+        public UniversityBuilder setYearOfFoundation(int yearOfFoundation) {
+            this.yearOfFoundation = yearOfFoundation;
+            return this;
+        }
+
+        public UniversityBuilder setMainProfile(University.StudyProfile mainProfile) {
+            this.mainProfile = mainProfile;
+            return this;
+        }
+
+        public University createUniversity() {
+            return new University(id, fullName, shortName, yearOfFoundation, mainProfile);
+        }
+
+        @Override
+        public String toString() {
+            return "UniversityBuilder{" +
+                    "id='" + id + '\'' +
+                    ", fullName='" + fullName + '\'' +
+                    ", shortName='" + shortName + '\'' +
+                    ", yearOfFoundation=" + yearOfFoundation +
+                    ", mainProfile=" + mainProfile +
+                    '}';
+        }
+    }
+}
