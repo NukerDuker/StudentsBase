49076d79cd0e41766d82b866d238f4641aa106b6
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-06-25 13:44:09 +0300

    Class Statistics created

---
+++ src/main/java/ru/studentsbase/model/Statistics.java
@@ -0,0 +1,17 @@
+package ru.studentsbase.model;
+
+import ru.studentsbase.enums.StudyProfile;
+
+public class Statistics {
+
+    private StudyProfile mainProfile;
+    private float avgExamScore;
+    private int studentQuantity;
+    private int universityQuantity;
+    private String universityFullName;
+
+    public Statistics() {
+    }
+
+
+}
---
+++ src/main/java/ru/studentsbase/service/XlsWriter.java
@@ -0,0 +1,4 @@
+package ru.studentsbase.service;
+
+public class XlsWriter {
+}
