1bd262b4d06f0a283a6dd9577a9207335b95d6ac
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-07-16 12:46:10 +0300

    Solve problem with Lombock constructors. Add builder with Lombok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import ru.studentsbase.enums.StudyProfile;
 import ru.studentsbase.model.Statistics;
 import ru.studentsbase.model.XmlModel;
 import ru.studentsbase.service.JsonWriter;
-import ru.studentsbase.service.XlsWriter;
 import ru.studentsbase.service.XmlWriter;
 import ru.studentsbase.util.Controller;
 import ru.studentsbase.comparators.students.StudentComparator;
@@ -14,15 +12,8 @@ import ru.studentsbase.enums.UnivCompareEnum;
 import ru.studentsbase.model.Student;
 import ru.studentsbase.service.ExcelReader;
 import ru.studentsbase.model.University;
-import ru.studentsbase.util.JsonUtil;
 import ru.studentsbase.util.StatsUtil;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
